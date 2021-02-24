1aac5df33a095e29e5faaf5afb915f0f090c3c9c
notscuffed <47509036+notscuffed@users.noreply.github.com>
2021-02-24 18:02:13 +0100

    Updated release.bat

--- release.bat
+++ release.bat
@@ -1,29 +1,36 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
 
-SET output=D:\Projects\Nuget
+SET output=D:\Nuget
 SET src=%~dp0
 SET release=bin\Release
+
+IF NOT "%NUGET_PACKAGES%"==""  (
+    SET nuget_cache=%NUGET_PACKAGES%
+) ELSE (
+    SET nuget_cache=%userprofile%\.nuget\packages
+)
+
+echo Nuget cache location: %nuget_cache%
 
 for %%l in (
     NotScuffed.Http
     NotScuffed.Threading
-	NotScuffed.Linq
-	NotScuffed.Strings
-	NotScuffed.IO
+    NotScuffed.Linq
+    NotScuffed.Strings
+    NotScuffed.IO
 ) do (
-
     :: Clean old release
     echo [90m* Cleaning old release for [35m%%l[0m
-    del /Q "%src%\%%l\%release%\*">nul
+    del /Q "%src%\%%l\%release%\*">nul 2>nul
 
     :: Clean nuget cache
     echo [90m* Cleaning nuget cache for [35m%%l[0m
-    del /S /Q "%userprofile%\.nuget\packages\%%l\">nul
+    del /S /Q !nuget_cache!\%%l\>nul 2>nul
 
     :: Compile libary
     echo [90m* Compiling [35m%%l[0m
-    dotnet pack --verbosity=quiet --configuration=Release "%src%\%%l">nul
+    dotnet pack --verbosity=quiet --configuration=Release "%src%\%%l">nul 2>nul
     if !ERRORLEVEL! NEQ 0 ( 
         echo [91mFailed to compile %%l[0m
         pause
@@ -32,10 +39,11 @@ for %%l in (
     
     :: Copy nuget package
     echo [90m* Copying [35m%%l[0m
-    copy /Y "%src%\%%l\%release%\*.nupkg" "%output%">nul
+    copy /Y "%src%\%%l\%release%\*.nupkg" "%output%">nul 2>nul
     if !ERRORLEVEL! NEQ 0 ( 
         echo [91mFailed to copy %%l[0m
         pause
         exit
     )
 )
+pause
